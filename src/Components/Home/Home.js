e517f4d48413d53b3e9f9ba97cd4e6d773cd10e6
Jennifer Lipps <jjlipps@gmail.com>
2020-01-25 13:46:59 -0600

    working on search feature

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,32 @@ import React from 'react';
 import Fuse from 'fuse.js';
 
 class Home extends React.Component {
+
+    searchOptions = {
+        shouldSort: true,
+        threshold: 0.6,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: [
+          "title",
+          "author",
+          "isbn"
+        ]
+      };
+
     render() {
-        return (d);
+        return (
+            <div className="Home">
+                <div className="Search-Input">
+                    <input
+                        type='search'                        
+                    />
+                </div>    
+            </div>
+        );
     };
 }
+
+export default Home;
